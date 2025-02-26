9619527571c3d551639ffc7ed3b3251054d63dd4
lmcay <lmicayanan@gmail.com>
2025-02-26 10:23:40 +0800

    Angular Mat Activiy 4

---
+++ angular_mat/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RegisterComponent, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular_mat';
+}
